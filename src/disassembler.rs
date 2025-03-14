f1e7ecc5ca46ed8e01787f78cedd744e4b41dab8
Olle Fredriksson <fredriksson.olle@gmail.com>
2025-03-14 18:40:20 +0100

    Jr

--- src/disassembler.rs
+++ src/disassembler.rs
@@ -97,6 +97,7 @@ impl Register {
 pub enum Instruction {
     Sll(Register, Register, u8),
     Srl(Register, Register, u8),
+    Jr(Register),
     Sync,
     Mult(Register, Register, Register),
     Addu(Register, Register, Register),
@@ -131,6 +132,7 @@ impl Instruction {
         (match self {
             Instruction::Sll(a, _, _) => Some(*a),
             Instruction::Srl(a, _, _) => Some(*a),
+            Instruction::Jr(_) => None,
             Instruction::Sync => None,
             Instruction::Mult(a, _, _) => Some(*a),
             Instruction::Addu(a, _, _) => Some(*a),
@@ -167,6 +169,7 @@ impl Instruction {
         (match self {
             Instruction::Sll(_, b, _) => [Some(*b), None],
             Instruction::Srl(_, b, _) => [Some(*b), None],
+            Instruction::Jr(a) => [Some(*a), None],
             Instruction::Sync => [None, None],
             Instruction::Mult(_, a, b) => [Some(*a), Some(*b)],
             Instruction::Addu(_, a, b) => [Some(*a), Some(*b)],
@@ -214,6 +217,7 @@ impl Display for Instruction {
         match self {
             Instruction::Sll(a, b, c) => write!(f, "sll {a}, {b}, {c}"),
             Instruction::Srl(a, b, c) => write!(f, "srl {a}, {b}, {c}"),
+            Instruction::Jr(a) => write!(f, "jr {a}"),
             Instruction::Sync => write!(f, "sync"),
             Instruction::Mult(a, b, c) => write!(f, "mult {a}, {b}, {c}"),
             Instruction::Addu(a, b, c) => write!(f, "addu {a}, {b}, {c}"),
@@ -271,6 +275,7 @@ pub fn disassemble(data: u32) -> Instruction {
         0b000000 => match data & 0b111111 {
             0b000000 => Instruction::Sll(rd, rt, shamt),
             0b000010 => Instruction::Srl(rd, rt, shamt),
+            0b001000 => Instruction::Jr(rs),
             0b001111 => Instruction::Sync,
             0b011000 => Instruction::Mult(rs, rt, rd),
             0b100001 => Instruction::Addu(rd, rs, rt),
