aed523686d80482f7efb6c5852d087588e62a81a
Olle Fredriksson <fredriksson.olle@gmail.com>
2025-03-15 16:29:21 +0100

    Sign extension trait

--- src/emotion_engine/interpreter.rs
+++ src/emotion_engine/interpreter.rs
@@ -1,5 +1,21 @@
 use super::{instruction::Instruction, state::State};
 
+trait SignExtend<T> {
+    fn sign_extend(self) -> T;
+}
+
+impl SignExtend<u64> for u32 {
+    fn sign_extend(self) -> u64 {
+        (self as i32).sign_extend()
+    }
+}
+
+impl SignExtend<u64> for i32 {
+    fn sign_extend(self) -> u64 {
+        self as i64 as u64
+    }
+}
+
 impl State {
     pub fn interpret(&mut self, instruction: &Instruction) {
         match instruction {
