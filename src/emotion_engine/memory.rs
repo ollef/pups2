882694d4d058e3a0162202b100a09c9d677c4fe6
Olle Fredriksson <fredriksson.olle@gmail.com>
2025-03-15 16:29:10 +0100

    Mock memory module

---
+++ src/emotion_engine/memory.rs
@@ -0,0 +1,11 @@
+struct Memory {}
+
+impl Memory {
+    pub fn new() -> Memory {
+        Memory {}
+    }
+
+    pub fn read32(&self, address: u32) -> u32 {
+        todo!("Implement Memory::read32")
+    }
+}
--- src/emotion_engine/mod.rs
+++ src/emotion_engine/mod.rs
@@ -1,5 +1,6 @@
 pub mod disassembler;
 pub mod instruction;
 pub mod interpreter;
+pub mod memory;
 pub mod register;
 pub mod state;
