11568024d72ee8e3cf76d5526872ded651aabae5
Olle Fredriksson <fredriksson.olle@gmail.com>
2025-03-16 02:18:09 +0100

    Remove old memory

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,24 @@
 mod emotion_engine;
 use elf::{endian::LittleEndian, ElfBytes};
 
-struct Memory {
-    pub rd: Vec<u8>,
-}
-
-impl Memory {
-    fn new() -> Self {
-        Self {
-            rd: vec![0; 1024 * 1024 * 32],
-        }
-    }
-}
-
 fn main() -> Result<(), std::io::Error> {
     let elf_data = std::fs::read("demos/demo2a.elf")?;
     let elf = ElfBytes::<LittleEndian>::minimal_parse(&elf_data).expect("Failed to parse ELF");
     let entry_point = elf.ehdr.e_entry as u32;
+    let mut state = emotion_engine::state::State::new(entry_point);
     println!("Entry point: {:x?}", entry_point);
     println!("Program header start: {:x?}", entry_point as u32);
-    let mut memory = Memory::new();
     for program_header in elf.segments().expect("Failed to get program headers") {
         let physical_memory_address = program_header.p_paddr;
+        let virtual_memory_address = program_header.p_vaddr;
         println!("Physical memory address: {:x?}", physical_memory_address);
+        println!("Virtual memory address: {:x?}", virtual_memory_address);
         let data = elf
             .segment_data(&program_header)
             .expect("Failed to get segment data");
-        memory.rd[physical_memory_address as usize..physical_memory_address as usize + data.len()]
-            .copy_from_slice(data);
+        // memory.rd[physical_memory_address as usize..physical_memory_address as usize + data.len()]
+        //     .copy_from_slice(data);
     }
-    let mut state = emotion_engine::state::State::new(entry_point);
     loop {
         state.step_interpreter();
     }
