e1d41d5d14d657dc662cbeeab17fe25f9bf06ecf
Olle Fredriksson <fredriksson.olle@gmail.com>
2025-03-22 00:51:37 +0100

    Refactor GS read/write

--- src/emotion_engine/gs.rs
+++ src/emotion_engine/gs.rs
@@ -50,10 +50,20 @@ impl Gs {
             signal_label_id: 0,
         }
     }
-
     pub fn write<T: Bytes>(&mut self, address: u32, value: T) {
-        assert!(std::mem::size_of::<T>() == 8);
-        let value = u64::from_bytes(value.to_bytes().as_ref());
+        match std::mem::size_of::<T>() {
+            8 => self.write64(address, u64::from_bytes(value.to_bytes().as_ref())),
+            _ => panic!("Invalid GS write size: {}", std::mem::size_of::<T>()),
+        }
+    }
+    pub fn read<T: Bytes>(&self, address: u32) -> T {
+        match std::mem::size_of::<T>() {
+            8 => T::from_bytes(self.read64(address).to_bytes().as_ref()),
+            _ => panic!("Invalid GS read size: {}", std::mem::size_of::<T>()),
+        }
+    }
+
+    pub fn write64(&mut self, address: u32, value: u64) {
         match address {
             0x1200_0000 => self.pcrtc_mode = value,
             0x1200_0010 => self.sync_mode1 = value,
@@ -78,28 +88,27 @@ impl Gs {
         }
     }
 
-    pub fn read<T: Bytes>(&self, address: u32) -> T {
-        assert!(std::mem::size_of::<T>() == 8);
+    pub fn read64(&self, address: u32) -> u64 {
         match address {
-            0x1200_0000 => T::from_bytes(&self.pcrtc_mode.to_bytes()),
-            0x1200_0010 => T::from_bytes(&self.sync_mode1.to_bytes()),
-            0x1200_0020 => T::from_bytes(&self.sync_mode2.to_bytes()),
-            0x1200_0030 => T::from_bytes(&self.dram_refresh.to_bytes()),
-            0x1200_0040 => T::from_bytes(&self.synch1.to_bytes()),
-            0x1200_0050 => T::from_bytes(&self.synch2.to_bytes()),
-            0x1200_0060 => T::from_bytes(&self.syncv.to_bytes()),
-            0x1200_0070 => T::from_bytes(&self.display_frame_buffer1.to_bytes()),
-            0x1200_0080 => T::from_bytes(&self.display1.to_bytes()),
-            0x1200_0090 => T::from_bytes(&self.display_frame_buffer2.to_bytes()),
-            0x1200_00A0 => T::from_bytes(&self.display2.to_bytes()),
-            0x1200_00B0 => T::from_bytes(&self.write_buffer.to_bytes()),
-            0x1200_00C0 => T::from_bytes(&self.write_data.to_bytes()),
-            0x1200_00D0 => T::from_bytes(&self.write_start.to_bytes()),
-            0x1200_00E0 => T::from_bytes(&self.background_color.to_bytes()),
-            0x1200_1000 => T::from_bytes(&self.status.to_bytes()),
-            0x1200_1010 => T::from_bytes(&self.interrupt_mask.to_bytes()),
-            0x1200_1040 => T::from_bytes(&self.bus_direction.to_bytes()),
-            0x1200_1080 => T::from_bytes(&self.signal_label_id.to_bytes()),
+            0x1200_0000 => self.pcrtc_mode,
+            0x1200_0010 => self.sync_mode1,
+            0x1200_0020 => self.sync_mode2,
+            0x1200_0030 => self.dram_refresh,
+            0x1200_0040 => self.synch1,
+            0x1200_0050 => self.synch2,
+            0x1200_0060 => self.syncv,
+            0x1200_0070 => self.display_frame_buffer1,
+            0x1200_0080 => self.display1,
+            0x1200_0090 => self.display_frame_buffer2,
+            0x1200_00A0 => self.display2,
+            0x1200_00B0 => self.write_buffer,
+            0x1200_00C0 => self.write_data,
+            0x1200_00D0 => self.write_start,
+            0x1200_00E0 => self.background_color,
+            0x1200_1000 => self.status,
+            0x1200_1010 => self.interrupt_mask,
+            0x1200_1040 => self.bus_direction,
+            0x1200_1080 => self.signal_label_id,
             _ => panic!("Invalid GS read64 from address: 0x{:08X}", address),
         }
     }
