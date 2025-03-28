c6126be54b327a7fb7c340be006d6013b29fea24
Olle Fredriksson <fredriksson.olle@gmail.com>
2025-03-28 20:54:56 +0100

    Move privileged registers to submodule

--- src/emotion_engine/gs/mod.rs
+++ src/emotion_engine/gs/mod.rs
@@ -3,8 +3,11 @@ use std::collections::VecDeque;
 use enum_map::Enum;
 use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
+use privileged_registers::PrivilegedRegisters;
 
 use crate::{bits::Bits, bytes::Bytes, fifo::Fifo, fix::Fix};
+
+mod privileged_registers;
 
 const LOCAL_MEMORY_SIZE: usize = 4 * 1024 * 1024;
 
@@ -23,29 +26,6 @@ struct Vertex {
 }
 
 type Fix124 = Fix<u16, 4>;
-
-#[derive(Debug, Default)]
-struct PrivilegedRegisters {
-    pcrtc_mode: u64,                           // PMODE
-    sync_mode1: u64,                           // SMODE1
-    sync_mode2: u64,                           // SMODE2
-    dram_refresh: u64,                         // SRFSH
-    synch1: u64,                               // SYNCH1
-    synch2: u64,                               // SYNCH2
-    syncv: u64,                                // SYNCV
-    display_frame_buffer1: DisplayFrameBuffer, // DISPFB1
-    display1: Display,                         // DISPLAY1
-    display_frame_buffer2: DisplayFrameBuffer, // DISPFB1
-    display2: Display,                         // DISPLAY1
-    write_buffer: u64,                         // EXTBUF
-    write_data: u64,                           // EXTDATA
-    write_start: u64,                          // EXTWRITE
-    background_color: u64,                     // BGCOLOR
-    status: u64,                               // CSR
-    interrupt_mask: u64,                       // IMR
-    bus_direction: u64,                        // BUSDIR
-    signal_label_id: u64,                      // SIGLBLID
-}
 
 #[derive(Debug, Default)]
 struct Registers {
@@ -144,70 +124,6 @@ impl Gs {
         let address = base_pointer + Self::psmct32_offset(x, y, width);
         self.local_memory[address as usize..address as usize + 4]
             .copy_from_slice(&value.to_bytes());
-    }
-
-    pub fn write_privileged<T: Bytes>(&mut self, address: u32, value: T) {
-        match std::mem::size_of::<T>() {
-            8 => self.write_privileged64(address, u64::from_bytes(value.to_bytes().as_ref())),
-            _ => panic!("Invalid GS write size: {}", std::mem::size_of::<T>()),
-        }
-    }
-    pub fn read_privileged<T: Bytes>(&self, address: u32) -> T {
-        match std::mem::size_of::<T>() {
-            8 => T::from_bytes(self.read_privileged64(address).to_bytes().as_ref()),
-            _ => panic!("Invalid GS read size: {}", std::mem::size_of::<T>()),
-        }
-    }
-
-    pub fn write_privileged64(&mut self, address: u32, value: u64) {
-        match address {
-            0x1200_0000 => self.privileged_registers.pcrtc_mode = value,
-            0x1200_0010 => self.privileged_registers.sync_mode1 = value,
-            0x1200_0020 => self.privileged_registers.sync_mode2 = value,
-            0x1200_0030 => self.privileged_registers.dram_refresh = value,
-            0x1200_0040 => self.privileged_registers.synch1 = value,
-            0x1200_0050 => self.privileged_registers.synch2 = value,
-            0x1200_0060 => self.privileged_registers.syncv = value,
-            0x1200_0070 => {
-                self.privileged_registers.display_frame_buffer1 = DisplayFrameBuffer::from(value);
-                println!(
-                    "Display frame buffer 1 = {:?}",
-                    self.privileged_registers.display_frame_buffer1
-                )
-            }
-            0x1200_0080 => {
-                self.privileged_registers.display1 = Display::from(value);
-                println!("Display 1 = {:?}", self.privileged_registers.display1)
-            }
-            0x1200_0090 => {
-                self.privileged_registers.display_frame_buffer2 = DisplayFrameBuffer::from(value);
-                println!(
-                    "Display frame buffer 2 = {:?}",
-                    self.privileged_registers.display_frame_buffer2
-                )
-            }
-            0x1200_00A0 => {
-                self.privileged_registers.display2 = Display::from(value);
-                println!("Display 2 = {:?}", self.privileged_registers.display2)
-            }
-            0x1200_00B0 => self.privileged_registers.write_buffer = value,
-            0x1200_00C0 => self.privileged_registers.write_data = value,
-            0x1200_00D0 => self.privileged_registers.write_start = value,
-            0x1200_00E0 => self.privileged_registers.background_color = value,
-            0x1200_1000 => self.privileged_registers.status = value,
-            0x1200_1010 => self.privileged_registers.interrupt_mask = value,
-            0x1200_1040 => self.privileged_registers.bus_direction = value,
-            0x1200_1080 => self.privileged_registers.signal_label_id = value,
-            _ => panic!("Invalid GS write64 {} to address: 0x{:08x}", value, address),
-        }
-    }
-
-    pub fn read_privileged64(&self, address: u32) -> u64 {
-        match address {
-            0x1200_1000 => self.privileged_registers.status,
-            0x1200_1080 => self.privileged_registers.signal_label_id,
-            _ => panic!("Invalid GS read64 from address: 0x{:08x}", address),
-        }
     }
 
     pub fn step(&mut self) {
@@ -623,53 +539,6 @@ impl Gs {
                 }
             }
             _ => todo!(),
-        }
-    }
-}
-
-#[derive(Debug, Default)]
-struct Display {
-    x_position: u16,              // DX
-    y_position: u16,              // DX
-    horizontal_magnification: u8, // MAGH
-    vertical_magnification: u8,   // MAGV
-    width: u16,                   // DW
-    height: u16,                  // DH
-}
-
-impl From<u64> for Display {
-    fn from(raw: u64) -> Self {
-        let horizontal_magnification = raw.bits(23..=26) as u8 + 1;
-        let vertical_magnification = raw.bits(27..=28) as u8 + 1;
-        Display {
-            x_position: raw.bits(0..=11) as u16 / horizontal_magnification as u16,
-            y_position: raw.bits(12..=22) as u16 / vertical_magnification as u16,
-            horizontal_magnification,
-            vertical_magnification,
-            width: (raw.bits(32..=43) as u16 + 1) / horizontal_magnification as u16,
-            height: (raw.bits(44..=54) as u16 + 1) / vertical_magnification as u16,
-        }
-    }
-}
-
-#[derive(Debug, Default)]
-struct DisplayFrameBuffer {
-    base_pointer: u32,
-    width: u16,
-    pixel_storage_format: PixelStorageFormat,
-    offset_x: u16,
-    offset_y: u16,
-}
-
-impl From<u64> for DisplayFrameBuffer {
-    fn from(raw: u64) -> Self {
-        DisplayFrameBuffer {
-            base_pointer: raw.bits(0..=8) as u32 * 2048,
-            width: raw.bits(9..=14) as u16 * 64,
-            pixel_storage_format: PixelStorageFormat::from_u64(raw.bits(15..=19))
-                .unwrap_or_else(|| panic!("Invalid pixel storage format {:b}", raw.bits(24..=29))),
-            offset_x: raw.bits(32..=42) as u16,
-            offset_y: raw.bits(43..=53) as u16,
         }
     }
 }
---
+++ src/emotion_engine/gs/privileged_registers.rs
@@ -0,0 +1,141 @@
+use num_traits::FromPrimitive;
+
+use crate::{bits::Bits, bytes::Bytes};
+
+use super::{Gs, PixelStorageFormat};
+
+#[derive(Debug, Default)]
+pub struct PrivilegedRegisters {
+    pub pcrtc_mode: u64,                           // PMODE
+    pub sync_mode1: u64,                           // SMODE1
+    pub sync_mode2: u64,                           // SMODE2
+    pub dram_refresh: u64,                         // SRFSH
+    pub synch1: u64,                               // SYNCH1
+    pub synch2: u64,                               // SYNCH2
+    pub syncv: u64,                                // SYNCV
+    pub display_frame_buffer1: DisplayFrameBuffer, // DISPFB1
+    pub display1: Display,                         // DISPLAY1
+    pub display_frame_buffer2: DisplayFrameBuffer, // DISPFB1
+    pub display2: Display,                         // DISPLAY1
+    pub write_buffer: u64,                         // EXTBUF
+    pub write_data: u64,                           // EXTDATA
+    pub write_start: u64,                          // EXTWRITE
+    pub background_color: u64,                     // BGCOLOR
+    pub status: u64,                               // CSR
+    pub interrupt_mask: u64,                       // IMR
+    pub bus_direction: u64,                        // BUSDIR
+    pub signal_label_id: u64,                      // SIGLBLID
+}
+
+#[derive(Debug, Default)]
+pub struct DisplayFrameBuffer {
+    pub base_pointer: u32,
+    pub width: u16,
+    pub pixel_storage_format: PixelStorageFormat,
+    pub offset_x: u16,
+    pub offset_y: u16,
+}
+
+impl From<u64> for DisplayFrameBuffer {
+    fn from(raw: u64) -> Self {
+        DisplayFrameBuffer {
+            base_pointer: raw.bits(0..=8) as u32 * 2048,
+            width: raw.bits(9..=14) as u16 * 64,
+            pixel_storage_format: PixelStorageFormat::from_u64(raw.bits(15..=19))
+                .unwrap_or_else(|| panic!("Invalid pixel storage format {:b}", raw.bits(24..=29))),
+            offset_x: raw.bits(32..=42) as u16,
+            offset_y: raw.bits(43..=53) as u16,
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct Display {
+    x_position: u16,              // DX
+    y_position: u16,              // DX
+    horizontal_magnification: u8, // MAGH
+    vertical_magnification: u8,   // MAGV
+    width: u16,                   // DW
+    height: u16,                  // DH
+}
+
+impl From<u64> for Display {
+    fn from(raw: u64) -> Self {
+        let horizontal_magnification = raw.bits(23..=26) as u8 + 1;
+        let vertical_magnification = raw.bits(27..=28) as u8 + 1;
+        Display {
+            x_position: raw.bits(0..=11) as u16 / horizontal_magnification as u16,
+            y_position: raw.bits(12..=22) as u16 / vertical_magnification as u16,
+            horizontal_magnification,
+            vertical_magnification,
+            width: (raw.bits(32..=43) as u16 + 1) / horizontal_magnification as u16,
+            height: (raw.bits(44..=54) as u16 + 1) / vertical_magnification as u16,
+        }
+    }
+}
+
+impl Gs {
+    pub fn write_privileged<T: Bytes>(&mut self, address: u32, value: T) {
+        match std::mem::size_of::<T>() {
+            8 => self.write_privileged64(address, u64::from_bytes(value.to_bytes().as_ref())),
+            _ => panic!("Invalid GS write size: {}", std::mem::size_of::<T>()),
+        }
+    }
+    pub fn read_privileged<T: Bytes>(&self, address: u32) -> T {
+        match std::mem::size_of::<T>() {
+            8 => T::from_bytes(self.read_privileged64(address).to_bytes().as_ref()),
+            _ => panic!("Invalid GS read size: {}", std::mem::size_of::<T>()),
+        }
+    }
+
+    pub fn write_privileged64(&mut self, address: u32, value: u64) {
+        match address {
+            0x1200_0000 => self.privileged_registers.pcrtc_mode = value,
+            0x1200_0010 => self.privileged_registers.sync_mode1 = value,
+            0x1200_0020 => self.privileged_registers.sync_mode2 = value,
+            0x1200_0030 => self.privileged_registers.dram_refresh = value,
+            0x1200_0040 => self.privileged_registers.synch1 = value,
+            0x1200_0050 => self.privileged_registers.synch2 = value,
+            0x1200_0060 => self.privileged_registers.syncv = value,
+            0x1200_0070 => {
+                self.privileged_registers.display_frame_buffer1 = DisplayFrameBuffer::from(value);
+                println!(
+                    "Display frame buffer 1 = {:?}",
+                    self.privileged_registers.display_frame_buffer1
+                )
+            }
+            0x1200_0080 => {
+                self.privileged_registers.display1 = Display::from(value);
+                println!("Display 1 = {:?}", self.privileged_registers.display1)
+            }
+            0x1200_0090 => {
+                self.privileged_registers.display_frame_buffer2 = DisplayFrameBuffer::from(value);
+                println!(
+                    "Display frame buffer 2 = {:?}",
+                    self.privileged_registers.display_frame_buffer2
+                )
+            }
+            0x1200_00A0 => {
+                self.privileged_registers.display2 = Display::from(value);
+                println!("Display 2 = {:?}", self.privileged_registers.display2)
+            }
+            0x1200_00B0 => self.privileged_registers.write_buffer = value,
+            0x1200_00C0 => self.privileged_registers.write_data = value,
+            0x1200_00D0 => self.privileged_registers.write_start = value,
+            0x1200_00E0 => self.privileged_registers.background_color = value,
+            0x1200_1000 => self.privileged_registers.status = value,
+            0x1200_1010 => self.privileged_registers.interrupt_mask = value,
+            0x1200_1040 => self.privileged_registers.bus_direction = value,
+            0x1200_1080 => self.privileged_registers.signal_label_id = value,
+            _ => panic!("Invalid GS write64 {} to address: 0x{:08x}", value, address),
+        }
+    }
+
+    pub fn read_privileged64(&self, address: u32) -> u64 {
+        match address {
+            0x1200_1000 => self.privileged_registers.status,
+            0x1200_1080 => self.privileged_registers.signal_label_id,
+            _ => panic!("Invalid GS read64 from address: 0x{:08x}", address),
+        }
+    }
+}
