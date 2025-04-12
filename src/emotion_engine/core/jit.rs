adf27d0b8fc682c7f9a33f4a37e5607afe66374c
Olle Fredriksson <fredriksson.olle@gmail.com>
2025-04-12 13:39:16 +0200

    Keep track of jitted code in virtual address space

--- src/emotion_engine/core/jit.rs
+++ src/emotion_engine/core/jit.rs
@@ -1,31 +1,29 @@
-use std::ops::Range;
+use std::{collections::BTreeMap, ops::Range};
 
-use crate::emotion_engine::{
-    bus::{BOOT_MEMORY_SIZE, MAIN_MEMORY_SIZE, SCRATCHPAD_SIZE},
-    dmac::{MemoryOrScratchpadAddress, MemoryOrScratchpadAddressView},
-};
+use bitvec::vec::BitVec;
 
-const ADDRESSES: usize = (MAIN_MEMORY_SIZE + BOOT_MEMORY_SIZE + SCRATCHPAD_SIZE) / 4;
-const FIRST_MAIN_MEMORY_INDEX: u32 = 0;
-const FIRST_BOOT_MEMORY_INDEX: u32 = FIRST_MAIN_MEMORY_INDEX + MAIN_MEMORY_SIZE as u32 / 4;
-const FIRST_SCRATCHPAD_INDEX: u32 = FIRST_BOOT_MEMORY_INDEX + BOOT_MEMORY_SIZE as u32 / 4;
-const MAX_CACHED: u16 = u16::MAX - 1;
-
-#[derive(Clone, Copy, PartialEq, Eq)]
-struct AddressIndex(u32);
-
-struct Jit {
-    address_cache_index: Box<[CacheIndex]>,
+pub struct Jit {
+    jitted_instructions: BitVec<usize>,
+    jitted_starts_map: BTreeMap<u32, u16>,
+    jitted_starts: Box<[CacheIndex]>,
     cache: Vec<Code>,
 }
 
 #[derive(Clone, Copy)]
 struct CacheIndex(u16);
 
+const CODE_CACHE_MAX_SIZE: u16 = u16::MAX - 2;
+
 #[derive(Clone, Copy, PartialEq, Eq)]
 enum CacheIndexView {
     NotCached,
     Cached(u16),
+    NotJittable,
+}
+
+enum Entry {
+    Vacant,
+    Occupied(Box<Code>),
 }
 
 impl From<CacheIndexView> for CacheIndex {
@@ -33,119 +31,123 @@ impl From<CacheIndexView> for CacheIndex {
         match value {
             CacheIndexView::NotCached => CacheIndex(0),
             CacheIndexView::Cached(index) => CacheIndex(index + 1),
+            CacheIndexView::NotJittable => CacheIndex(u16::MAX),
         }
     }
 }
 
 impl CacheIndex {
     fn view(self) -> CacheIndexView {
-        if self.0 == 0 {
-            CacheIndexView::NotCached
-        } else {
-            CacheIndexView::Cached(self.0 - 1)
+        match self.0 {
+            0 => CacheIndexView::NotCached,
+            u16::MAX => CacheIndexView::NotJittable,
+            _ => CacheIndexView::Cached(self.0 - 1),
         }
     }
 }
 
 struct Code {
-    address_index_range: Range<AddressIndex>,
+    address_range: Range<u32>,
 }
+
+const VIRTUAL_MEMORY_SIZE: usize = 0x1_0000_0000;
+const INSTRUCTION_SIZE: usize = std::mem::size_of::<u32>();
 
 impl Jit {
     pub fn new() -> Self {
         Jit {
-            address_cache_index: vec![CacheIndex::from(CacheIndexView::NotCached); ADDRESSES]
-                .into_boxed_slice(),
+            jitted_instructions: BitVec::from_vec(vec![
+                0;
+                VIRTUAL_MEMORY_SIZE
+                    / INSTRUCTION_SIZE
+                    / (std::mem::size_of::<usize>() * 8)
+            ]),
+            jitted_starts_map: BTreeMap::new(),
+            jitted_starts: Box::new([CacheIndex(0); VIRTUAL_MEMORY_SIZE / INSTRUCTION_SIZE]),
             cache: Vec::new(),
         }
     }
 
-    pub fn code(&mut self, address: MemoryOrScratchpadAddress) -> &Code {
-        let address_index = Self::address_index(address);
-        match self.address_cache_index[address_index.0 as usize].view() {
-            CacheIndexView::NotCached => {
-                if self.cache.len() >= MAX_CACHED as usize {
-                    self.remove_from_cache(0);
-                }
-                let code = self.compile_from(address);
-                let cache_index = CacheIndex::from(CacheIndexView::Cached(self.cache.len() as u16));
-                for i in code.address_index_range.start.0..code.address_index_range.end.0 {
-                    let slot = &mut self.address_cache_index[i as usize];
-                    assert!(slot.view() == CacheIndexView::NotCached);
-                    *slot = cache_index;
-                }
-                self.cache.push(code);
-                &self.cache[self.cache.len() - 1]
-            }
-            CacheIndexView::Cached(cache_index) => {
-                let code = &self.cache[cache_index as usize];
-                if code.address_index_range.start == address_index {
-                    return &self.cache[cache_index as usize];
-                }
-                for i in code.address_index_range.start.0..code.address_index_range.end.0 {
-                    self.address_cache_index[i as usize] =
-                        CacheIndex::from(CacheIndexView::NotCached);
-                }
-                let new_code = self.compile_from(address);
-                for i in new_code.address_index_range.start.0..new_code.address_index_range.end.0 {
-                    self.address_cache_index[i as usize] =
-                        CacheIndex::from(CacheIndexView::Cached(cache_index));
-                }
-                let code = &mut self.cache[cache_index as usize];
-                *code = new_code;
-                code
-            }
+    fn remove(&mut self, cache_index: u16) {
+        let code = self.cache.swap_remove(cache_index as usize);
+        if let Some(moved_code) = self.cache.last() {
+            let moved_index = (self.cache.len() - 1) as u16;
+            self.jitted_starts_map
+                .insert(moved_code.address_range.start, moved_index)
+                .unwrap();
+            self.jitted_starts[moved_code.address_range.start as usize / INSTRUCTION_SIZE] =
+                CacheIndex::from(CacheIndexView::Cached(moved_index));
+        }
+        self.jitted_starts[code.address_range.start as usize / INSTRUCTION_SIZE] =
+            CacheIndex::from(CacheIndexView::NotCached);
+        self.jitted_starts_map
+            .remove(&code.address_range.start)
+            .unwrap();
+        let start = self
+            .jitted_starts_map
+            .range(..code.address_range.start)
+            .rev()
+            .map(|(_, index)| {
+                let earlier_code = &self.cache[*index as usize];
+                earlier_code.address_range.end
+            })
+            .take_while(|earlier_code_end| *earlier_code_end > code.address_range.start)
+            .max()
+            .unwrap_or(code.address_range.start);
+        let end = self
+            .jitted_starts_map
+            .range(code.address_range.start..code.address_range.end)
+            .next()
+            .map(|(start, _)| *start)
+            .unwrap_or(code.address_range.end);
+        if start < end {
+            self.jitted_instructions
+                [start as usize / INSTRUCTION_SIZE..end as usize / INSTRUCTION_SIZE]
+                .fill(false);
         }
     }
 
-    fn compile_from(&mut self, address: MemoryOrScratchpadAddress) -> Code {
-        todo!()
+    fn add(&mut self, code: Code) {
+        if self.cache.len() as u16 == CODE_CACHE_MAX_SIZE {
+            self.remove(0);
+        }
+        let cache_index = self.cache.len() as u16;
+        self.jitted_starts[code.address_range.start as usize / INSTRUCTION_SIZE] =
+            CacheIndex::from(CacheIndexView::Cached(cache_index));
+        self.jitted_starts_map
+            .insert(code.address_range.start, cache_index);
+        self.jitted_instructions[code.address_range.start as usize / INSTRUCTION_SIZE
+            ..code.address_range.end as usize / INSTRUCTION_SIZE]
+            .fill(true);
+        self.cache.push(code);
     }
 
-    fn remove_from_cache(&mut self, cache_index: u16) {
-        let old = self.cache.swap_remove(cache_index as usize);
-        for i in old.address_index_range.start.0..old.address_index_range.end.0 {
-            self.address_cache_index[i as usize] = CacheIndex::from(CacheIndexView::NotCached);
+    #[inline(always)]
+    pub fn invalidate_range(&mut self, range: Range<u32>) {
+        if self.jitted_instructions[range.start as usize / INSTRUCTION_SIZE
+            ..(range.end as usize).div_ceil(INSTRUCTION_SIZE)]
+            .not_any()
+        {
+            return;
         }
-        if let Some(moved) = self.cache.get(cache_index as usize) {
-            for i in moved.address_index_range.start.0..moved.address_index_range.end.0 {
-                self.address_cache_index[i as usize] =
-                    CacheIndex::from(CacheIndexView::Cached(cache_index));
-            }
-        }
+
+        self.invalidate_range_slow(range);
     }
 
-    fn invalidate_address(&mut self, address: MemoryOrScratchpadAddress) {
-        let address_index = Self::address_index(address);
-        match self.address_cache_index[address_index.0 as usize].view() {
-            CacheIndexView::NotCached => {}
-            CacheIndexView::Cached(cache_index) => self.remove_from_cache(cache_index),
-        }
-    }
+    fn invalidate_range_slow(&mut self, range: Range<u32>) {
+        let to_remove = self
+            .jitted_starts_map
+            .range(..range.end)
+            .rev()
+            .take_while(|(_, index)| {
+                let code = &self.cache[**index as usize];
+                code.address_range.end > range.start
+            })
+            .map(|(_, index)| *index)
+            .collect::<Vec<_>>();
 
-    fn address_index(address: MemoryOrScratchpadAddress) -> AddressIndex {
-        match address.view() {
-            MemoryOrScratchpadAddressView::Memory(address) => {
-                assert!(address & (std::mem::size_of::<u32>() - 1) as u32 == 0);
-                match address {
-                    0x0000_0000..0x1000_0000 => {
-                        let address = address & (MAIN_MEMORY_SIZE as u32 - 1);
-                        AddressIndex(FIRST_MAIN_MEMORY_INDEX + (address / 4))
-                    }
-                    0x1FC0_0000..0x2000_0000 => {
-                        let address = address & (BOOT_MEMORY_SIZE as u32 - 1);
-                        AddressIndex(FIRST_BOOT_MEMORY_INDEX + (address / 4))
-                    }
-                    _ => {
-                        panic!("Invalid read at address: 0x{:08x}", address);
-                    }
-                }
-            }
-            MemoryOrScratchpadAddressView::Scratchpad(address) => {
-                assert!(address & (std::mem::size_of::<u32>() - 1) as u32 == 0);
-                let address = address & (SCRATCHPAD_SIZE as u32 - 1);
-                AddressIndex(FIRST_SCRATCHPAD_INDEX + (address / 4))
-            }
+        for index in to_remove {
+            self.remove(index);
         }
     }
 }
