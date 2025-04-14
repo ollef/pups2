35bc7adddfca3b46e18dc75b5f3668e356918836
Olle Fredriksson <fredriksson.olle@gmail.com>
2025-04-14 20:10:55 +0200

    Jit daddu

--- src/emotion_engine/core/jit.rs
+++ src/emotion_engine/core/jit.rs
@@ -816,12 +816,10 @@ impl<'a> JitCompiler<'a> {
                 }
                 Instruction::Dadd(_, _, _) => todo!(),
                 Instruction::Daddu(rd, rs, rt) => {
-                    // let value = self
-                    //     .get_register::<u64>(rs)
-                    //     .wrapping_add(self.get_register::<u64>(rt));
-                    // self.set_register(rd, value);
-                    unhandled();
-                    break;
+                    let rs_value = self.get_register(rs, Size::S64);
+                    let rt_value = self.get_register(rt, Size::S64);
+                    let value = self.function_builder.ins().iadd(rs_value, rt_value);
+                    self.set_register(rd, value, Size::S64);
                 }
                 Instruction::Dsub(_, _, _) => todo!(),
                 Instruction::Dsubu(_, _, _) => todo!(),
