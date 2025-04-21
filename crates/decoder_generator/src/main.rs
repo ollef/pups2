93924950b688d2475a85e4325b576e00ea4e389a
Olle Fredriksson <fredriksson.olle@gmail.com>
2025-04-21 21:22:45 +0200

    wip

--- crates/decoder_generator/src/main.rs
+++ crates/decoder_generator/src/main.rs
@@ -1,3 +1,221 @@
+use std::{
+    collections::BTreeMap,
+    fmt::{Debug, Display, Formatter},
+    ops::Range,
+};
+
+use yaml_rust2::{Yaml, YamlLoader};
+
+#[derive(Debug)]
+struct Encoding<T> {
+    bits: u32,
+    mask: u32,
+    payload: T,
+}
+
+impl<T: Display> Display for Encoding<T> {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        for bit in (0..32).rev() {
+            if bit == 5 || bit == 10 || bit == 15 || bit == 20 || bit == 25 {
+                write!(f, " ")?;
+            }
+            let mask = (self.mask >> bit) & 1;
+            let bits = (self.bits >> bit) & 1;
+            if mask == 0 {
+                write!(f, ".")?;
+            } else if bits == 0 {
+                write!(f, "0")?;
+            } else {
+                write!(f, "1")?;
+            }
+        }
+        write!(f, ": {}", self.payload)?;
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+enum DecisionTree<T> {
+    Leaf(T),
+    Match {
+        bits: Range<u32>,
+        nodes: BTreeMap<u32, DecisionTree<T>>,
+    },
+}
+
+struct InstructionFormat<'a>(&'a str);
+
+impl InstructionFormat<'_> {
+    pub fn mnemonic(&self) -> &str {
+        if let Some(eq_index) = self.0.find('=') {
+            self.0[eq_index + 1..]
+                .split_ascii_whitespace()
+                .next()
+                .unwrap()
+        } else {
+            self.0.split_ascii_whitespace().next().unwrap()
+        }
+    }
+
+    pub fn operands(&self) -> impl Iterator<Item = &str> {
+        self.0.match_indices("{").map(|(i, _)| {
+            let end = self.0[i..].find('}').expect("Missing closing brace");
+            let braced = &self.0[i + 1..i + end];
+            if let Some(colon) = braced.find(':') {
+                &braced[..colon]
+            } else {
+                braced
+            }
+        })
+    }
+}
+
+impl<T: Clone + Display> DecisionTree<T> {
+    pub fn new(encodings: &[Encoding<T>]) -> Self {
+        if encodings.is_empty() {
+            panic!("Cannot create a decision tree with no encodings");
+        }
+        if encodings.len() == 1 && encodings[0].mask == 0 {
+            return DecisionTree::Leaf(encodings[0].payload.clone());
+        }
+
+        let mut discriminant_mask: u32 = !0;
+        for encoding in encodings.iter() {
+            discriminant_mask &= encoding.mask;
+        }
+        if discriminant_mask == 0 {
+            println!("No discriminating bits in encodings:");
+            for encoding in encodings.iter() {
+                println!("{}", encoding);
+            }
+            panic!("No discriminating bits in encodings");
+        }
+
+        let range_start = discriminant_mask.trailing_zeros();
+        let range_end = range_start + (discriminant_mask >> range_start).trailing_ones();
+        let discriminant_mask =
+            !((1 << range_start) - 1) & (1u32.checked_shl(range_end).unwrap_or(0)).wrapping_sub(1);
+
+        println!("Discriminant mask: {:#034b}", discriminant_mask);
+        println!("Range start: {}", range_start);
+        println!("Range end: {}", range_end);
+
+        let mut nodes: BTreeMap<u32, Vec<Encoding<T>>> = BTreeMap::new();
+        for encoding in encodings.iter() {
+            let bits = (encoding.bits & discriminant_mask) >> range_start;
+            nodes.entry(bits).or_default().push(Encoding {
+                bits: encoding.bits,
+                mask: encoding.mask & !discriminant_mask,
+                payload: encoding.payload.clone(),
+            });
+        }
+
+        let nodes = nodes
+            .into_iter()
+            .map(|(bits, encodings)| {
+                let node = DecisionTree::new(&encodings);
+                (bits, node)
+            })
+            .collect();
+        DecisionTree::Match {
+            bits: range_start..range_end,
+            nodes,
+        }
+    }
+}
+
+fn opcode_visitor_trait(encodings: &[Encoding<String>]) {
+    println!("pub trait OpcodeVisitor {{");
+    println!("    type Output;");
+    let mut unknown_added = false;
+    for encoding in encodings {
+        let format = InstructionFormat(&encoding.payload);
+        let mnemonic = format.mnemonic();
+        if mnemonic == "unknown" {
+            if unknown_added {
+                continue;
+            }
+            unknown_added = true;
+        }
+
+        println!("    fn {mnemonic}(self, instruction: Instruction) -> Self::Output;");
+    }
+    println!("}}");
+}
+
+fn instruction_visitor_trait(operands: &Yaml, encodings: &[Encoding<String>]) {
+    println!("pub trait InstructionVisitor {{");
+    println!("    type Output;");
+    let mut unknown_added = false;
+    for encoding in encodings {
+        let format = InstructionFormat(&encoding.payload);
+        let mnemonic = format.mnemonic();
+        if mnemonic == "unknown" {
+            if unknown_added {
+                continue;
+            }
+            unknown_added = true;
+        }
+
+        print!("    fn {mnemonic}(self");
+        for operand in format.operands() {
+            let operand_type = operands[operand]["type"].as_str().unwrap();
+            print!(", {operand}: {operand_type}");
+        }
+
+        println!(") -> Self::Output;");
+    }
+    println!("}}");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let arg = std::env::args().nth(1).unwrap();
+    let string = std::fs::read_to_string(arg).unwrap();
+    let yaml = YamlLoader::load_from_str(&string).unwrap();
+    // println!("{:#?}", yaml);
+    let raw_instructions = &yaml[0]["instructions"].as_hash().unwrap();
+    let mut encodings = Vec::with_capacity(raw_instructions.len());
+    for (encoding_str, format) in raw_instructions.iter() {
+        println!("{:#?}", encoding_str);
+        println!("{:#?}", format);
+        println!(
+            "mnemonic: {}",
+            InstructionFormat(format.as_str().unwrap()).mnemonic()
+        );
+        let mut encoding = Encoding {
+            bits: 0,
+            mask: 0,
+            payload: format.as_str().unwrap().to_string(),
+        };
+        for char in encoding_str.as_str().unwrap().chars() {
+            match char {
+                ' ' => continue,
+                '0' => {
+                    encoding.bits <<= 1;
+                    encoding.mask <<= 1;
+                    encoding.bits |= 0;
+                    encoding.mask |= 1;
+                }
+                '1' => {
+                    encoding.bits <<= 1;
+                    encoding.mask <<= 1;
+                    encoding.bits |= 1;
+                    encoding.mask |= 1;
+                }
+                '.' => {
+                    encoding.bits <<= 1;
+                    encoding.mask <<= 1;
+                    encoding.bits |= 0;
+                    encoding.mask |= 0;
+                }
+                _ => panic!("Invalid character in encoding: {}", char),
+            }
+        }
+        println!("{}", encoding);
+        encodings.push(encoding);
+    }
+    let decision_tree = DecisionTree::new(&encodings);
+    println!("{:#?}", decision_tree);
+    opcode_visitor_trait(&encodings);
+    instruction_visitor_trait(&yaml[0]["operands"], &encodings);
 }
--- crates/pups2/src/emotion_engine/core/execution.rs
+++ crates/pups2/src/emotion_engine/core/execution.rs
@@ -9,7 +9,7 @@ impl Core {
             if self.state.delayed_branch_target.is_some() {
                 cycles -= 1;
                 let instruction = decode(self.read_virtual(bus, self.state.program_counter));
-                println!("I {:08x}: {}", self.state.program_counter, instruction);
+                // println!("I {:08x}: {}", self.state.program_counter, instruction);
                 self.interpret_instruction(instruction, bus);
             } else {
                 let cache_entry = self.jit.cache_entry(&self.state, &self.mmu, bus, self.mode);
@@ -17,13 +17,13 @@ impl Core {
                 match &cache_entry.code {
                     Code::Jitted(function) => {
                         let bytes = cache_entry.address_range.end - cache_entry.address_range.start;
-                        for pc in (self.state.program_counter..self.state.program_counter + bytes)
-                            .step_by(4)
-                        {
-                            let physical_address = self.mmu.virtual_to_physical(pc, self.mode);
-                            let instruction = decode(bus.read(physical_address));
-                            println!("J {:08x}: {}", pc, instruction);
-                        }
+                        // for pc in (self.state.program_counter..self.state.program_counter + bytes)
+                        //     .step_by(4)
+                        // {
+                        //     let physical_address = self.mmu.virtual_to_physical(pc, self.mode);
+                        //     let instruction = decode(bus.read(physical_address));
+                        //     println!("J {:08x}: {}", pc, instruction);
+                        // }
                         assert!(self.mmu.physically_consecutive(
                             self.state.program_counter..self.state.program_counter + bytes,
                             self.mode
@@ -34,7 +34,7 @@ impl Core {
                     Code::Interpreted(instruction) => {
                         cycles -= 1;
                         let instruction = *instruction;
-                        println!("I {:08x}: {}", self.state.program_counter, instruction);
+                        // println!("I {:08x}: {}", self.state.program_counter, instruction);
                         self.interpret_instruction(instruction, bus);
                     }
                 }
