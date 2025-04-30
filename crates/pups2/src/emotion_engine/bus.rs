67fda95ac6f21344cf7668f1b2b74760a6b8211e
Olle Fredriksson <fredriksson.olle@gmail.com>
2025-04-30 21:32:43 +0200

    Unhandle a couple of addresses

--- crates/pups2/src/emotion_engine/bus.rs
+++ crates/pups2/src/emotion_engine/bus.rs
@@ -124,6 +124,10 @@ impl Bus {
                         // println!("Read from DMAC: 0x{:08x}==0x{:08x}", address, result);
                         result
                     }
+                    0x1000_F130 => {
+                        println!("Unhandled read at: 0x{:08x}", address);
+                        T::default()
+                    }
                     0x1200_0000..0x1201_0000 => {
                         let result = self.gs.read_privileged(address);
                         // println!("Read from GS: 0x{:08x}==0x{:08x}", address, result);
@@ -177,8 +181,8 @@ impl Bus {
                         // println!("Write to DMAC: 0x{:08x}:=0x{:08x}", address, value);
                         self.dmac.write(address, value)
                     }
-                    0x1000_F100 | 0x1000_F120 | 0x1000_F140 | 0x1000_F150 | 0x1000_F500
-                    | 0x1F80_1470 | 0x1F80_1472 => {
+                    0x1000_F100 | 0x1000_F120 | 0x1000_F140 | 0x1000_F150 | 0x1000_F180
+                    | 0x1000_F500 | 0x1F80_1470 | 0x1F80_1472 => {
                         println!("Unhandled write: 0x{:08x}:=0x{:08x}", address, value);
                     }
                     0x1200_0000..0x1201_0000 => {
