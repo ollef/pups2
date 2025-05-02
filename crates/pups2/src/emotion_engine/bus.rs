54f97d5a0decad94ab0631b4895ff990511e47e4
Olle Fredriksson <fredriksson.olle@gmail.com>
2025-05-02 19:42:33 +0200

    Unhandle another address

--- crates/pups2/src/emotion_engine/bus.rs
+++ crates/pups2/src/emotion_engine/bus.rs
@@ -182,7 +182,7 @@ impl Bus {
                         self.dmac.write(address, value)
                     }
                     0x1000_F100 | 0x1000_F120 | 0x1000_F140 | 0x1000_F150 | 0x1000_F180
-                    | 0x1000_F500 | 0x1F80_1470 | 0x1F80_1472 => {
+                    | 0x1000_F410 | 0x1000_F500 | 0x1F80_1470 | 0x1F80_1472 => {
                         println!("Unhandled write: 0x{:08x}:=0x{:08x}", address, value);
                     }
                     0x1200_0000..0x1201_0000 => {
